f97779c861b55dbfbc6327121fbc4dd6d8465fdc
croath <croathliu@gmail.com>
2013-02-28 17:21:40 +0800

    CRSlotLabel class file created.

---
+++ slotLabelExample/slotLabelExample/CRSlotLabel.h
@@ -0,0 +1,13 @@
+//
+//  CRSlotLabel.h
+//  slotLabelExample
+//
+//  Created by croath on 13-2-28.
+//  Copyright (c) 2013年 croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRSlotLabel : UILabel
+
+@end
---
+++ slotLabelExample/slotLabelExample/CRSlotLabel.m
@@ -0,0 +1,31 @@
+//
+//  CRSlotLabel.m
+//  slotLabelExample
+//
+//  Created by croath on 13-2-28.
+//  Copyright (c) 2013年 croath. All rights reserved.
+//
+
+#import "CRSlotLabel.h"
+
+@implementation CRSlotLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
